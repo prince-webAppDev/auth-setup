3126a9a1973798cde933187af184864f7924e3d2
rgstartup <rgstartup1@gmail.com>
2026-02-09 18:31:38 +0530

    ...

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -2,11 +2,10 @@ import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    tailwindcss:{},
-    autoprefixer:{}
-  },
+  plugins: [
+    tailwindcss(),
+    autoprefixer()
+  ]
 };
 
 export default config;
